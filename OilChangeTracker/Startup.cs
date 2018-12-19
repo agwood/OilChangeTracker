8b239e4e18554764111ad8411060ec533ac54d6a
agwod <aaron@aaronwood.net>
2018-12-19 11:04:24 -0700

    Add project files.

---
+++ OilChangeTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OilChangeTracker.Startup))]
+namespace OilChangeTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

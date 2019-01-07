6c660e145c908646466672d0f5ff0cab02007446
agwod <aaron@aaronwood.net>
2019-01-07 07:57:58 -0700

    Initial

---
+++ OilChangeTracker/DataContexts/IdentityDb.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using OilChangeTracker.Entities;
+using OilChangeTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OilChangeTracker.DataContexts
+{
+    public class IdentityDb : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDb()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static IdentityDb Create()
+        {
+            return new IdentityDb();
+        }
+    }
+}

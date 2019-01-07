0853c150d48a9d770ae3fd5ef64be162df3c474c
agwod <aaron@aaronwood.net>
2019-01-07 10:24:55 -0700

    Added create  vehicles page

---
+++ OilChangeTracker/Controllers/VehiclesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNet.Identity;
+using OilChangeTracker.DataContexts;
+using OilChangeTracker.Models;
+using OilChangeTracker.ViewModels;
+using System.Web.Mvc;
+
+namespace OilChangeTracker.Controllers
+{
+
+    public class VehiclesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VehiclesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Vehicles
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new VehicleFormViewModel();
+            
+            return View(viewModel);
+        }
+
+        // POST: Vehicles
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(VehicleFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", viewModel);
+            }
+            var vehicle = new Vehicle
+            {
+                Nickname = viewModel.Nickname,
+                Vin = viewModel.Vin,
+                Make = viewModel.Make,
+                Model = viewModel.Model,
+                Year = viewModel.Year,
+                Color = viewModel.Color,
+                OwnerId = User.Identity.GetUserId()
+            };
+
+            _context.Vehicles.Add(vehicle);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+
+
+}

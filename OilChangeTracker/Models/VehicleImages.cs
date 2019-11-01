4240edb8262731686f9a183ba7cbe017c975d9e8
Aaron Wood <aaron@aaronwood.net>
2019-11-01 08:50:56 -0700

    update front-page verbiage to make it clear you have to login to the site to see the application

---
+++ OilChangeTracker/Models/VehicleImages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OilChangeTracker.Models
+{
+    public class VehicleImages
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Vehicle")]
+        public Vehicle VehicleId { get; set; }
+
+
+
+    }
+}
